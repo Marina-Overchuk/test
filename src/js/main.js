e12e4cbc4a8f927ecd7c20e2837a9db4887d4124
@marinaOverchuk <marinaover@ukr.net>
2024-03-09 23:28:50 +0200

    edited

--- src/js/main.js
+++ src/js/main.js
@@ -1,131 +1,119 @@
-
 (function ($) {
-  $(document).ready( function () {
+  $(document).ready(function () {
 
-// HEADER-BURGER ========================================================================================================================================================
+    // HEADER-BURGER ========================================================================================================================================================
 
-  $('.header__burger').on('click', function () {
-  
-    if($(this).hasClass('active')){
-      $(this).toggleClass("active");
-      $('body').css('overflow', 'auto');
-      $('header').addClass('header_mobile');
-      $('.container__header-menu').removeClass('open')
-    }
-    
-    else{
-      $('header').removeClass('header_mobile')
-      $(this).toggleClass("active");
-      $('body').css('overflow', 'hidden');
-      $('.container__header-menu').addClass('open')
-    }
-    
-  });
-  
+    $('.header__burger').on('click', function () {
 
-//sub-menu
-$(".menu-item-has-children").click(function() {
-  if (window.matchMedia('(max-width: 1279px)').matches) {
-  $(".sub-menu").not($(this).find(".sub-menu")).removeClass("open");
-  $(this).find(".sub-menu").toggleClass("open");
-  
-  $(".menu-item-has-children").not($(this)).removeClass('open');
-  $(this).toggleClass('open');
-  }
-});  
+      if ($(this).hasClass('active')) {
+        $(this).toggleClass("active");
+        $('body').css('overflow', 'auto');
+        $('header').addClass('header_mobile');
+        $('.container__header-menu').removeClass('open')
+      } else {
+        $('header').removeClass('header_mobile')
+        $(this).toggleClass("active");
+        $('body').css('overflow', 'hidden');
+        $('.container__header-menu').addClass('open')
+      }
+
+    });
 
 
-// HEADER-SCROLL ========================================================================================================================================================
+    //sub-menu
+    $(".menu-item-has-children").click(function () {
+      if (window.matchMedia('(max-width: 1279px)').matches) {
+        $(".sub-menu").not($(this).find(".sub-menu")).removeClass("open");
+        $(this).find(".sub-menu").toggleClass("open");
+
+        $(".menu-item-has-children").not($(this)).removeClass('open');
+        $(this).toggleClass('open');
+      }
+    });
+
+
+    // HEADER-SCROLL ========================================================================================================================================================
 
     let scrollChange = 5;
-        
+
     function headerScroll(params) {
-      
+
       let scroll = $(window).scrollTop();
-      
+
       if (scroll >= scrollChange) {
         $('.header').addClass('header_scroll');
-      }
-      else {
+      } else {
         $('.header').removeClass('header_scroll');
       }
     }
 
-    $(document).on('scroll', ()=>{
-      headerScroll();  
+    $(document).on('scroll', () => {
+      headerScroll();
     })
-  
+
     headerScroll();
 
-  // LANGUAGE BUTTONS =============================================================================
+    // LANGUAGE BUTTONS =============================================================================
 
-  $('.lang-link').click(function() {
+    $('.lang-link').click(function () {
 
-    $('.lang-link').removeClass('active');
+      $('.lang-link').removeClass('active');
 
-    $(this).addClass('active');
-    
-  });
+      $(this).addClass('active');
 
-  //FAQ ===============================================================================
-  $('.accordion-header').click(function() {
-
-    $(this).toggleClass('active').next('.accordion-content').slideToggle();
-    
-    $('.accordion-header').not(this).removeClass('active').next('.accordion-content').slideUp();
-  });
-
-  //APPEND SOC ICON ==========================================================
-  function appendSocIcon() {
-    if (window.matchMedia("(min-width: 576px)").matches) {
-        $('.header__soc-icon').insertAfter('.header__language')
-      }
-    else {
-      $('.header__soc-icon').appendTo('.header-menu__wrapper')
-    }
-  }
-
-  appendSocIcon();
-
-  $(window).on('resize', () => {
-    appendSocIcon(); 
-  });
-
-//Animation h1 hero section
-gsap.registerPlugin(ScrollTrigger);
-
-const text = document.querySelectorAll('.text');
-
-text.forEach(elem => {
-
-  const splitText = (el) => {
-    el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
-    return `<span class="word">` +
-        m.replace(/(-|#|@)?\S(-|#|@)?/g, "<span class='letter'>$&</span>") +
-        `</span>`;
     });
-    return el;
-  };
 
-  const split = splitText(elem);
+    //FAQ ===============================================================================
+    $('.accordion-header').click(function () {
 
-});
+      $(this).toggleClass('active').next('.accordion-content').slideToggle();
 
-gsap.from('h1.hero__title span.letter', {
-  opacity: 0,
-  y: 30,
-  duration: 0.8,
-  stagger: 0.06,
-});
+      $('.accordion-header').not(this).removeClass('active').next('.accordion-content').slideUp();
+    });
+
+    //APPEND SOC ICON ==========================================================
+    function appendSocIcon() {
+      if (window.matchMedia("(min-width: 576px)").matches) {
+        $('.header__soc-icon').insertAfter('.header__language')
+      } else {
+        $('.header__soc-icon').appendTo('.header-menu__wrapper')
+      }
+    }
+
+    appendSocIcon();
+
+    $(window).on('resize', () => {
+      appendSocIcon();
+    });
+
+    //Animation h1 hero section
+    gsap.registerPlugin(ScrollTrigger);
+
+    const text = document.querySelectorAll('.text');
+
+    text.forEach(elem => {
+
+      const splitText = (el) => {
+        el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
+          return `<span class="word">` +
+            m.replace(/(-|#|@)?\S(-|#|@)?/g, "<span class='letter'>$&</span>") +
+            `</span>`;
+        });
+        return el;
+      };
+
+      const split = splitText(elem);
+
+    });
+
+    gsap.from('h1.hero__title span.letter', {
+      opacity: 0,
+      y: 30,
+      duration: 0.8,
+      stagger: 0.06,
+    });
 
 
 
-
-});
+  });
 })(jQuery);
-
-
-
-
-
-
